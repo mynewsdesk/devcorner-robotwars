223caf906d00375f56401296c96962fb7360a909
Thomas Pfeiffer <thomas.pfeiffer@mynewsdesk.com>
2015-06-25 15:02:06 +0200

    even better

--- bots/aimbot.rb
+++ bots/aimbot.rb
@@ -17,21 +17,25 @@ class Aimbot
       @turn_direction == change_direction
     end
 
-    if has_aim?
-      @hits_ago = 0
-    else
-      @hits_ago += 1
-    end
-
-    if has_aim? || @hits_ago < 10
-      if close_distance?
-        fire 3
-      else
-        fire 1
+    if enemy_seen_recently(50)
+      if gun_heat == 0
+        say "--FRESH MEAT!--"
+        case @last_enemy_dist
+        when 0...100
+          say "--YOU DIE!--"
+          fire 3
+        when 100...500
+          say "--MUAHAHA!--"
+          fire 2
+        when 500...1000
+          say "--BANG!--"
+          fire 0.5
+        else fire 0.2
+        end
       end
     else
-      fire 0.3
-      turn_gun 3 - actual_direction
+      turn_gun 10
+      fire 0.1
     end
 
     accelerate 1 unless speed == 8
@@ -41,14 +45,33 @@ class Aimbot
     if x > (battlefield_width - 80) || x < 80 || y > (battlefield_height - 80) || y < 80
       10
     elsif has_aim?
-      0
+      3
     else
       turn_direction
     end
   end
 
+  def enemy_seen_recently(since = 100)
+    enemy_seen
+    @last_enemy_seen ||= 0
+    (time - @last_enemy_seen) < since
+  end
+
+  def enemy_seen
+    was_seen = has_aim?
+    @last_enemy_dist ||= 0
+    if was_seen
+      @last_enemy_seen = time
+      @last_enemy_dist = events["robot_scanned"][0][0].to_i
+    end
+    was_seen
+  end
+
   def has_aim?
-    !events['robot_scanned'].empty?
+    !events["robot_scanned"].empty?
+  end
+
+  def spread_fire power
   end
 
   def change_direction
